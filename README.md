62c39da450d7f3f3304a4128754c852f9c8585ea
peterkang <peterkang2001@gmail.com>
2020-02-25 17:43:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+艾尔扎克(Isac)
+
+海斗士之一，是整个Poseidon2项目的一部分，用Springboot实现的管理后台API
---
+++ src/main/java/cn/peterkang/isac/IsacApplication.java
@@ -0,0 +1,20 @@
+package cn.peterkang.isac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class IsacApplication {
+    @RequestMapping("hello")
+    String Hello(){
+        return "Hello peter";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsacApplication.class, args);
+    }
+
+}
